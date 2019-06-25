66d6522931f25f9c03bbf682abd7b34569b6f6a5
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-06-25 15:06:50 +0200

    passing parameters

---
+++ examples/part1/05_passing_parameters/kotlin/sample.kts
@@ -0,0 +1,9 @@
+//fun greet(name: String): String { 
+//  return "Hello $name" 
+//}
+
+//fun greet(name: String): String = "Hello $name" 
+
+fun greet(name: String) = "Hello $name" 
+
+println(greet("Jane"))
